48d619844f19365a7fd13fdb5f8157258a274c98
Sylvain Hallé <shalle@acm.org>
2016-12-18 17:16:58 -0500

    Fix

--- Source/Parsing/src/ca/uqac/lif/bullwinkle/BnfParser.java
+++ Source/Parsing/src/ca/uqac/lif/bullwinkle/BnfParser.java
@@ -27,8 +27,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import ca.uqac.lif.bullwinkle.BnfRule.InvalidRuleException;
 import ca.uqac.lif.util.EmptyException;
